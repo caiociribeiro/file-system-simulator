92958faa99962722b9ef8a28053d7d1ef5b15eec
Caio Ciriaco <caiociriacorib@gmail.com>
2025-11-25 10:39:57 -0300

    remove unused methods

--- src/FSException.java
+++ src/FSException.java
@@ -1,23 +1,5 @@
 public class FSException extends Exception {
-
     public FSException(String msg) {
         super(msg);
-    }
-
-    public static String alreadyExists(String name) {
-        return Color.RED + String.format("Error: %s already exists.", name) + Color.RESET;
-    }
-
-    public static String notFound(String name) {
-        return Color.RED + String.format("Error: %s not found.", name) + Color.RESET;
-    }
-
-    public static String notADirectory(String name) {
-        return Color.RED + String.format("Error: %s is not a directory.", name) + Color.RESET;
-    }
-
-    public static String invalidName(String name) {
-        return Color.RED + String.format("Error: '%s' is not a valid name for a file or directory.", name)
-                + Color.RESET;
     }
 }
